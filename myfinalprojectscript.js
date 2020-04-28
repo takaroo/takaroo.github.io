b578e3872baaf85d0830a6d310687b0e9ac99482
takaroo <bagizita@gmail.com>
2020-04-28 17:20:43 +0200

    j

--- myfinalprojectscript.js
+++ myfinalprojectscript.js
@@ -1,7 +1,47 @@
+
 $(document).ready(function() {
-    alert("document ready occurred!");
-    $('.pictureBox').append('<img src="1.jpg" class="mainImg">');
+    //alert("document ready occurred!");
+    let data = {
+            photo: ['1', '2', '3', '4', '5',],
+            title: ['My Title 1', 'My Title 2', 'My Title 3', 'My Title 4', 'My Title 5',],
+            description: ['What happened here, why is this a very nice image', 'Curabitur eget lacus pharetra, efficitur dui ac, hendrerit nisi. Pellentesque finib', 'um lacinia. Nam finibus justo in nisl consequat, eu aliquam nisi accumsan. Nam a cursus felis, vel blandit lectus. Quisque non dui vel ma', 'ue. Vestibulum vitae ligula vitae lacus mattis porta at ut velit. Pellentesque sagittis, dui a placerat ullamcorper, augue ex tincidunt leo, ac', 'si. Pellentesque finibus interdum lacinia. Nam finibus justo in nisl consequat, eu aliquam nisi accumsan. Nam a cursus felis, vel blandit lect',]
+      };
+
+    var currentPhoto = 0;
+    var currentPhotoTitel = 0;
+    var currentPhotoDescription = 0;
+
+    $('.pictureBox').append('<img src="'+data.photo[0]+'.jpg" class="mainImg">');
+
+    $('#currentPhotoTitel').text(data.title[currentPhotoTitel]);
+    $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
     for(var i = 2; i <= 5; i++) {
     $('.thumbnailsBox').append('<div class="thumbnails"><img src="'+i+'.jpg" class="thumbnailimg"></div>');
      }
+
+     $('.one').click(() => {
+        currentPhoto--;
+        currentPhotoTitel--;
+        currentPhotoDescription--;
+        $('.pictureBox').append('<img src="'+currentPhoto+'.jpg" class="mainImg">');
+        $('#currentPhotoTitel').text(data.title[currentPhotoTitel]);
+        $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
+    });
+
+    $('.two').click(() => {
+        currentPhoto++;
+        currentPhotoTitel++;
+        currentPhotoDescription++;
+        $('.pictureBox').append('<img src="'+currentPhoto+'.jpg" class="mainImg">');
+        $('#currentPhotoTitel').text(data.title[currentPhotoTitel]);
+        $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
+        console.log(currentPhoto);
+        if (currentPhoto=5) {
+
+        }
+
+        
+    });
+
+
 });
