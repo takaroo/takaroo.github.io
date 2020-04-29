d117cf78729fe29d710d350a8d83d7a1a21da8fd
takaroo <bagizita@gmail.com>
2020-04-29 17:21:42 +0200

    d

--- myfinalprojectscript.js
+++ myfinalprojectscript.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-    //alert("document ready occurred!");
     let data = {
             photo: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',],
             title: ['My Title 1', 'My Title 2', 'My Title 3', 'My Title 4', 'My Title 5',],
@@ -10,20 +9,28 @@ $(document).ready(function() {
     var currentPhoto = 0;
     var currentPhotoTitel = 0;
     var currentPhotoDescription = 0;
-    //$('.pictureBox img').attr('src', data.photo[currentPhoto]);
-
-    //$('.pictureBox').append('<img src="'+data.photo[0]+'.jpg" class="mainImg">');
+        
     let loadPhoto = (photoNumber) => {
 
         $('#mainImg').attr('src', data.photo[currentPhoto]);
+        $('#mainImg').css('border', 'solid 2px white');
+        $('#mainImg').css('box-shadow', '0 0 50px #fff');
         $('#currentPhotoTitel').text(data.title[currentPhotoTitel]);
         $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
+
     }
-        loadPhoto(currentPhoto);
+        
 
     for(var i = 1; i <= data.photo.length; i++) {
-    $('.thumbnailsBox').append('<div class="thumbnails"><img src="'+i+'.jpg" class="thumbnailimg"></div>');
-     }
+    $('.thumbnailsBox').append('<div class="thumbnails"><img src="'+i+'.jpg" class="thumbnailimg" data-index="${index}"></div>');
+    
+    var numberIndex = parseInt(currentPhoto+1);
+   $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('border', 'solid 2px white');
+   $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('box-shadow', '0 0 50px #fff');
+
+}
+
+     loadPhoto(currentPhoto);
 
      $('.one').click(() => {
         if (0 < currentPhoto){
@@ -31,19 +38,50 @@ $(document).ready(function() {
             currentPhotoTitel--;
             currentPhotoDescription--;
             loadPhoto(currentPhoto);
+
+            var numberIndex = parseInt(currentPhoto+1);
+           $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('border', 'solid 2px white');
+           $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('box-shadow', '0 0 50px #fff');
+
+           var numberIndex=numberIndex+1;
+
+            $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('border', '');
+            $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('box-shadow', '');
+          
         }
     });
 
   
 
     $('.two').click(() => { 
+        
        if (currentPhoto < 4){
             currentPhoto++;
             currentPhotoTitel++;
             currentPhotoDescription++;
+           
             loadPhoto(currentPhoto);
+
+            var numberIndex = parseInt(currentPhoto+1);
+           $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('border', 'solid 2px white');
+           $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('box-shadow', '0 0 50px #fff');
+
+           var numberIndex=numberIndex-1
+
+            $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('border', '');
+            $('.thumbnails img[src="'+numberIndex+'.jpg"]').css('box-shadow', '');
+
+
+
         }
-    });
+
+        });
+      
+
+        
+
+        
+    
         
 
 
