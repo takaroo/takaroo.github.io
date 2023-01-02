abe07f532d74c78a0020e83a83456230aef897ce
takaroo <44847178+takaroo@users.noreply.github.com>
2023-01-02 16:44:24 +0100

    Add files via upload

---
+++ 17/script.js
@@ -0,0 +1,16 @@
+/*
+
+npm: node package manager 
+ javascript done in modules ->  bundling to join them -> polyfilling with Parcel -> goes into a javascript bundle
+
+ Module: reusable piece of code, standalone file usually, it can have imports and exports, what we expose is the public API, parts can possibly be developed independent from each other ~ isolating components, leads to a more organized code base
+ dependencies
+
+ parsing modules
+ */
+
+// Importing module
+
+import './shoppingCart.js';
+
+console.log('Importing module');
